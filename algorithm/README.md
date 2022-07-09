5784ca82613ddb169e80e6c6851d35b773749dc8
jimin <dbwlals97@naver.com>
2022-07-09 23:59:15 +0900

    feat: 124 나라

--- algorithm/README.md
+++ algorithm/README.md
@@ -1,4 +1,10 @@
 알고리즘/자료구조 정리
-----------------
+=================
 
+그래프
+-----
 [union find](union_find.md)
+
+#### 최소 신장 트리 (MST)
+[kruskal](kruskal.md) : union find 알고리즘 사용
+[prims](prims.md)
--- programmers/12899.py
+++ programmers/12899.py
@@ -1,23 +1,17 @@
-from functools import reduce
-
+# 124나라
 def to_124(n):
     str_124 = lambda x : ['1','2','4'][x]
 
     subtractor = 0
     divider = 1
-    arr = []
+    result = ''
     while n >= subtractor:
         digit = (n - subtractor) % ( divider * 3 ) // divider
-        arr.append(str_124(digit))
+        result = str_124(digit) + result
 
         divider *= 3
         subtractor += divider
-    
-    # return ''.join(arr[::-1])
-    return reduce(lambda a, x: x+a, arr)
-
-for x in range(1, 20):
-    print(x, to_124(x-1))
+    return result
 
 def solution(x):
     answer = to_124(x-1)
