4bc09ca07a7f91f83379b812145ef45849280d92
jimin <dbwlals97@naver.com>
2022-09-15 21:21:29 +0900

    fix: 설명 추가

--- leetcode/543.py
+++ leetcode/543.py
@@ -1,7 +1,12 @@
+# 포인트는 None인 노드의 리턴값을 -1로 할 것!
+# 그래야 leaf 에서 
+# diameter = -1 + -1 + 2 = 0
+# depth = max(-1, -1) + 1 = 0
+
 class Solution:
     diameter = 0
     
-    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+    def diameterOfBinaryTree(self, root) -> int:
         def depth(node):
             if not node:
                 return -1
