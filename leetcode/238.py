f1af68d3325dd16eea3fcdef13303f81eacba867
jimin <dbwlals97@naver.com>
2022-10-12 01:26:19 +0900

    fix: 238 풀이2

--- leetcode/238.py
+++ leetcode/238.py
@@ -14,3 +14,23 @@ class Solution:
         length = len(nums)
         result = [prod_left[i] * prod_right[length-i-1] for i in range(length)]
         return result
+
+
+# extra memory 사용 안하는 버전
+# -> 방향 <- 방향으로 한번씩 순회하면서 최종 array 에 multiply
+class Solution:
+    def productExceptSelf(self, nums):
+        result = []
+        pre = 1
+        
+        for x in nums:
+            result.append(pre)
+            pre*=x
+        
+        length=len(nums)
+        post = 1
+        for i, x in enumerate(nums[::-1]):
+            result[length-1-i]*=post
+            post*=x
+            
+        return result
