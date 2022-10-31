bbaa5d9b3e2f6036950d4d5e8f21173cf717dfd3
유지민 <86466319+jimin-yu@users.noreply.github.com>
2022-11-01 08:36:35 +0900

    feat: 33 second solution

--- leetcode/33.py
+++ leetcode/33.py
@@ -22,3 +22,29 @@ class Solution:
                 
         l, r = 0, len(nums)-1
         return bsearch(l, r)
+
+
+# sol2)
+class Solution:
+    def search(self, nums, target: int) -> int:
+        l, r = 0, len(nums)-1
+        
+        while l<=r:
+            mid = l+(r-l)//2
+            if nums[mid]==target:
+                return mid
+            
+            # mid is in left sorted portion
+            if nums[l] <= nums[mid]:
+                if nums[l] <= target < nums[mid]:
+                    r = mid-1
+                else:
+                    l = mid+1
+            # mid is in right sorted portion
+            else:
+                if nums[mid] < target <= nums[r]:
+                    l = mid+1
+                else:
+                    r = mid-1
+        
+        return -1
